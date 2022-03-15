8289f1bd5988d4af553b6a5d1f61963ed188722e
Alaire Sena <truongsondotcom@gmail.com>
2022-03-15 10:15:25 +0700

    Test auto build Github

---
+++ .vuepress/config.ts
@@ -0,0 +1,33 @@
+import { defineHopeConfig } from "vuepress-theme-hope";
+import themeConfig from "./themeConfig";
+
+export default defineHopeConfig({
+  base: "/",
+
+  dest: "./dist",
+
+  head: [
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
+      },
+    ],
+  ],
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Alaire Sena's blog",
+      description: "My personal blog site.",
+    },
+    "/vi/": {
+      lang: "vi-VN",
+      title: "主题演示",
+      description: "vuepress-theme-hope 的演示",
+    },
+  },
+
+  themeConfig,
+});
